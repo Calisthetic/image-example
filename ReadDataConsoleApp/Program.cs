43063f1891dafd27253697f0674f8952d501be9d
Calisthetic <vovakalash04@mail.ru>
2023-12-26 11:23:07 +0500

    Init data

--- ReadDataConsoleApp/Program.cs
+++ ReadDataConsoleApp/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ReadDataConsoleApp.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +11,29 @@ namespace ReadDataConsoleApp
 {
     internal class Program
     {
+        private static ImagesDBEntities _context = new ImagesDBEntities();
         static void Main(string[] args)
         {
+            string imagesPath = @"C:\Users\admin\Downloads\09_1_2-2022_2_\09_1.2-2022_2\Вариант 2\Вариант 2\Сессия 1\Сотрудники_import";
+
+            var employees = _context.Employees.ToList();
+            var files = Directory.GetFiles(imagesPath);
+
+            for (int i = 0; i < employees.Count(); i++)
+            {
+                foreach (var file in files)
+                {
+                    if (file.Contains(employees[i].SecondName.ToString()))
+                    {
+                        employees[i].ImageFile = File.ReadAllBytes(file);
+                        break;
+                    }
+                }
+            }
+
+            _context.SaveChanges();
+
+            Console.ReadLine();
         }
     }
 }
---
+++ ReadMe.md
@@ -0,0 +1,20 @@
+<h1 align="center">Work with Images example</h1>
+
+## Description
+
+**ReadDataConsoleApp** - Console application to read image bytes and save them to database (ssms)
+
+## How to setup
+
+
+
+## Others
+
+### Script entire database
+
+1. Select database
+2. Open context menu using RMB
+3. Tasks => Generate Scripts
+4. Follow the options below
+
+![ScriptDB](./ReadmeImages/ScriptDatabase.png)
